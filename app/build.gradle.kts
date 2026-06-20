0293f4db67bbd7e4e0cf4f71dfdd95c21c580a90
zarakowskigrzegorz-ai <zarakowski.grzegorz@gmail.com>
2026-06-20 20:01:25 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
-  alias(libs.plugins.android.application)
-  alias(libs.plugins.kotlin.compose)
-  alias(libs.plugins.google.devtools.ksp)
-  alias(libs.plugins.roborazzi)
-  alias(libs.plugins.secrets)
-alias(libs.plugins.google.dagger.hilt)
+    alias(libs.plugins.android.application)
+    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.devtools.ksp)
+    alias(libs.plugins.roborazzi)
+    alias(libs.plugins.secrets)
+    alias(libs.plugins.google.dagger.hilt)
 }
 
 android {
