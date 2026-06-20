600cad69ef07bb06720d58338a6c07454f01ee5e
zarakowskigrzegorz-ai <zarakowski.grzegorz@gmail.com>
2026-06-20 20:47:55 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
+    id("com.android.application") version "8.4.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
-    alias(libs.plugins.google.dagger.hilt) apply false
+    id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
+    id("com.google.dagger.hilt.android") version "2.51.1" apply false
 }
