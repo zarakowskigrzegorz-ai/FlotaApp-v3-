f4f49e4332416a6ee872fea7c451c33fcf5bcd12
zarakowskigrzegorz-ai <zarakowski.grzegorz@gmail.com>
2026-06-20 21:59:37 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application") version "8.4.0" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false // <--- To jest kluczowe
     id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
 }
